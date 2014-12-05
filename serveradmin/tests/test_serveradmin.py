a0a72776b868b0c6130eebe9e9ba4ad9d791e5e3
Marc Delage <mdelage@student.42.fr>
2014-12-05 16:13:08 +0100

    remove useless test

--- serveradmin/tests/test_serveradmin.py
+++ serveradmin/tests/test_serveradmin.py
@@ -7,10 +7,6 @@ from serveradmin.utils import call_service
 pytestmark = pytest.mark.django_db
 
 
-def test_services_page(app):
-    assert app.get('/server/services/', status=200)
-
-
 def test_services_page_in_GET_mode(app):
     assert app.get(reverse('server:services'), status=200)
 
