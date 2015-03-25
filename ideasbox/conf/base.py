945e561bf11185aaa3de40d8b4cd466e7ef0bd2d
Yohan Boniface <yb@enix.org>
2015-03-25 12:40:46 +0200

    Display current server version (fix #108)

--- ideasbox/conf/base.py
+++ ideasbox/conf/base.py
@@ -71,7 +71,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
-    "ideasbox.context_processors.settings"
+    "ideasbox.context_processors.settings",
+    "ideasbox.context_processors.version",
 )
 
 ROOT_URLCONF = 'ideasbox.urls'
--- ideasbox/context_processors.py
+++ ideasbox/context_processors.py
@@ -1,6 +1,12 @@
 from django.conf import settings as djsettings
 
+from ideasbox import __version__
+
 
 def settings(request):
     keys = ['IDEASBOX_NAME']
     return {k: getattr(djsettings, k) for k in keys}
+
+
+def version(request):
+    return {'IDEASBOX_VERSION': __version__}
