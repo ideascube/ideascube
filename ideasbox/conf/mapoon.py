f5ddb4b629367eb7cc6e5162f1200fb6a705b2dc
Yohan Boniface <yb@enix.org>
2015-07-14 14:02:47 +0200

    Remove is_sent_to_school from Mapoon settings (cf #151)

--- ideasbox/conf/mapoon.py
+++ ideasbox/conf/mapoon.py
@@ -12,6 +12,6 @@ USER_FORM_FIELDS = (
     ('Ideasbox', ['serial', 'box_awareness']),
     (_('Personal informations'), ['short_name', 'full_name', 'birth_year', 'gender', 'id_card_number']),  # noqa
     (_('Family'), ['marital_status', 'family_status', 'children_under_12', 'children_under_18', 'children_above_18']),  # noqa
-    (_('In the town'), ['current_occupation', 'school_level', 'is_sent_to_school']),  # noqa
+    (_('In the town'), ['current_occupation', 'school_level']),
     (_('Language skills'), ['en_level']),
 )
