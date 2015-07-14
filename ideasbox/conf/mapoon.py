1f8a5e78e252d6611d633fed9635411d73d6055b
Yohan Boniface <yb@enix.org>
2015-07-14 09:53:52 +0200

    0.1.1

--- ideasbox/conf/mapoon.py
+++ ideasbox/conf/mapoon.py
@@ -10,7 +10,7 @@ LANGUAGE_CODE = 'en'
 MONITORING_ENTRY_EXPORT_FIELDS = ['serial', 'user_id', 'birth_year', 'gender']
 USER_FORM_FIELDS = (
     ('Ideasbox', ['serial', 'box_awareness']),
-    (_('Personal informations'), ['short_name', 'full_name', 'latin_name', 'birth_year', 'gender', 'id_card_number']),  # noqa
+    (_('Personal informations'), ['short_name', 'full_name', 'birth_year', 'gender', 'id_card_number']),  # noqa
     (_('Family'), ['marital_status', 'family_status', 'children_under_12', 'children_under_18', 'children_above_18']),  # noqa
     (_('In the town'), ['current_occupation', 'school_level', 'is_sent_to_school']),  # noqa
     (_('Language skills'), ['en_level']),
