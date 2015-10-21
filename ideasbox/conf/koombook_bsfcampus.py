2e8e2781af8c744199f9ff08d409df58e14a186f
fheslouin <florian.heslouin@zimdeo.fr>
2015-10-21 12:59:25 +0200

    Add IDEASBOX_ID var in ideasbox.init

--- ideasbox/conf/koombook_bsfcampus.py
+++ ideasbox/conf/koombook_bsfcampus.py
@@ -6,18 +6,18 @@ from .base import *  # noqa
 from django.utils.translation import ugettext_lazy as _
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG', False))
+DEBUG = bool(os.environ.get('DEBUG', True))
 
 TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = ['.koombook.lan.', 'localhost']
+ALLOWED_HOSTS = ['.koombook.lan.', 'localhost', '127.0.0.1']
 
 LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'Europe/Paris'
 
 # Ideas Box specifics
-STORAGE_ROOT = '/media/hdd/ideasbox/storage' 
+STORAGE_ROOT = '/media/hdd/ideasServer/storage' 
 
 IDEASBOX_NAME = 'BSF Campus'
 
