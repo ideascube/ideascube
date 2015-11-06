5f6dfb8d6cf37bf6237e18423799c94dd2741ee3
fheslouin <florian.heslouin@zimdeo.fr>
2015-11-06 12:04:25 +0100

    Add conf file for ifb burundi

---
+++ ideascube/conf/kb_ifb_bdi.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+"""KoomBook conf"""
+
+from .base import *  # noqa
+from django.utils.translation import ugettext_lazy as _
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = bool(os.environ.get('DEBUG', True))
+
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['.koombook.lan.', 'localhost', '127.0.0.1']
+
+LANGUAGE_CODE = 'fr'
+
+TIME_ZONE = None
+
+# Ideas Box specifics
+STORAGE_ROOT = '/media/hdd/ideascube/storage' 
+
+IDEASCUBE_NAME = 'Institut Français Burundi'
+
+DOMAIN = 'koombook.lan'
+
+BACKUP_FORMAT = 'gztar'
+
+STAFF_HOME_CARDS = [c for c in STAFF_HOME_CARDS if c['url'] in ['user_list', 'server:power', 'server:backup']]
+
+HOME_CARDS = STAFF_HOME_CARDS + [
+    {
+        'id': 'blog',
+    },
+    {
+        'id': 'mediacenter',
+    },
+    {
+        'id': 'wikipedia',
+    },
+    {
+        'id': 'khanacademy',
+    },
+    {
+        'id': 'vikidia',
+    },
+    {
+        'id': 'appinventor',
+    },
+    {
+        'id': 'gutenberg',
+    },
+]
+
+IDEASCUBE_BODY_ID = 'koombook'
