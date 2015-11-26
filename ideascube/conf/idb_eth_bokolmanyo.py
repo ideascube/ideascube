15dcae5dc13ae21452f6bbb3a7c8947f99c1d515
Yohan Boniface <yb@enix.org>
2015-11-26 08:45:05 +0100

    Add Ethiopian conf (Bokolmanyo and Melkadida)

---
+++ ideascube/conf/idb_eth_bokolmanyo.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Bokolmanyo box in Ethiopia"""
+from .base import *  # noqa
+from django.utils.translation import ugettext_lazy as _
+
+IDEASCUBE_NAME = u"Bokolmanyo"
+COUNTRIES_FIRST = ['SO', 'ET']
+TIME_ZONE = 'Africa/Addis_Ababa'
+LANGUAGE_CODE = 'en'
+MONITORING_ENTRY_EXPORT_FIELDS = ['serial', 'refugee_id', 'birth_year',
+                                  'gender']
+USER_FORM_FIELDS = (
+    ('Ideasbox', ['serial', 'box_awareness']),
+    (_('Personal informations'), ['refugee_id', 'short_name', 'full_name', 'latin_name', 'birth_year', 'gender']),  # noqa
+    (_('Family'), ['marital_status', 'family_status', 'children_under_12', 'children_under_18', 'children_above_18']),  # noqa
+    (_('In the camp'), ['camp_entry_date', 'camp_activities', 'current_occupation', 'camp_address']),  # noqa
+    (_('Origin'), ['country', 'city', 'country_of_origin_occupation', 'school_level', 'is_sent_to_school']),  # noqa
+    (_('Language skills'), ['ar_level', 'en_level']),
+    (_('National residents'), ['id_card_number']),
+)
---
+++ ideascube/conf/idb_eth_melkadida.py
@@ -0,0 +1,4 @@
+"""Melkadida box in Ethiopia"""
+from idb_eth_bokolmanyo import *
+
+IDEASCUBE_NAME = u"Melkadida"
