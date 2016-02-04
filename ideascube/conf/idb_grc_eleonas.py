2f0bb3c8478e080b4f5d3becda43d5a9e5e7f8ef
Yohan Boniface <yb@enix.org>
2016-02-04 16:58:51 +0100

    Update Greece conf files

---
+++ ideascube/conf/idb_grc_eleonas.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""Library Without Borders box in Athenes, Grece"""
+from .idb import *  # noqa
+from django.utils.translation import ugettext_lazy as _
+
+IDEASCUBE_NAME = u"Eleonas"
+IDEASCUBE_PLACE_NAME = _("camp")
+COUNTRIES_FIRST = ['AF', 'GR', 'IR', 'PK', 'SY']
+TIME_ZONE = 'Europe/Athens'
+LANGUAGE_CODE = 'en'
+LOAN_DURATION = 14
+USER_FORM_FIELDS = (
+    (_('Personal informations'), ['serial', 'short_name', 'full_name', 'latin_name', 'birth_year', 'gender']),  # noqa
+    (_('In the town'), ['current_occupation', 'school_level', 'phone', 'email']),  # noqa
+    (_('Language skills'), ['en_level', 'ar_level', 'fa_level']),
+)
+
+HOME_CARDS = STAFF_HOME_CARDS + [
+    {
+        'id': 'blog',
+    },
+    {
+        'id': 'library',
+    },
+    {
+        'id': 'mediacenter',
+    },
+    {
+        'id': 'wikipedia',
+    },
+    {
+        'id': 'khanacademy',
+    },
+    {
+        'id': 'gutenberg',
+    },
+]
---
+++ ideascube/conf/idb_grc_moria.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Library Without Borders box in Lesbos, GR"""
+from .idb_grc_eleonas import *  # noqa
+
+IDEASCUBE_NAME = u"Moria"
