147ff246a2a4626284d54f9e90c2868d5e25a432
Mathieu Bridon <bochecha@daitauha.fr>
2016-02-10 16:54:14 +0100

    catalog: Introduce the content manager cli
    We are implementing a content manager, which will be able to download
    and install apps and content coming from remote sources.
    
    This commit adds a new Django command, which lets us manage remote
    sources for the catalog.

---
+++ ideascube/serveradmin/management/commands/catalog.py
@@ -0,0 +1,64 @@
+import argparse
+
+from django.core.management.base import BaseCommand, CommandError
+
+from ideascube.serveradmin.catalog import Catalog
+
+
+class Command(BaseCommand):
+    help = 'Manage apps and content'
+
+    def add_arguments(self, parser):
+        subs = parser.add_subparsers(
+            title='Commands', dest='cmd', metavar='',
+            parser_class=argparse.ArgumentParser)
+
+        # -- Manage remote sources --------------------------------------------
+        remote = subs.add_parser('remotes', help='Manage remote sources')
+
+        remotesubs = remote.add_subparsers(title='Commands', dest='remotecmd')
+        remotesubs.required = True
+
+        list = remotesubs.add_parser('list', help='List known remote sources')
+        list.set_defaults(func=self.list_remotes)
+
+        add = remotesubs.add_parser('add', help='Add a remote source')
+        add.add_argument('id', help='The id for the new remote source')
+        add.add_argument('name', help='The name for the new remote source')
+        add.add_argument('url', help='The url for the new remote source')
+        add.set_defaults(func=self.add_remote)
+
+        rm = remotesubs.add_parser('remove', help='Remove a remote source')
+        rm.add_argument('id', help='The id of the remote source to remove')
+        rm.set_defaults(func=self.remove_remote)
+
+        self.parser = parser
+
+    def handle(self, *args, **options):
+        if 'func' not in options:
+            self.parser.print_help()
+            self.parser.exit(1)
+
+        self.catalog = Catalog()
+        options['func'](options)
+
+    # -- Manage remote sources ------------------------------------------------
+    def list_remotes(self, options):
+        for remote in self.catalog.list_remotes():
+            left = '[{0.id}] {0.name}'.format(remote)
+            print('{0:>35} : {1.url}'.format(left, remote))
+
+    def add_remote(self, options):
+        try:
+            self.catalog.add_remote(
+                options['id'], options['name'], options['url'])
+
+        except ValueError as e:
+            raise CommandError(e)
+
+    def remove_remote(self, options):
+        try:
+            self.catalog.remove_remote(options['id'])
+
+        except ValueError as e:
+            raise CommandError(e)
---
+++ ideascube/serveradmin/tests/test_catalog_command.py
@@ -0,0 +1,144 @@
+from operator import itemgetter
+
+from django.core.management import call_command
+from django.core.management.base import CommandError
+import pytest
+import yaml
+
+
+def test_no_command(tmpdir, settings, capsys):
+    with pytest.raises(SystemExit):
+        call_command('catalog')
+
+    out, err = capsys.readouterr()
+    assert out.strip().startswith('usage: ')
+    assert err.strip() == ''
+
+
+def test_add_remote(tmpdir, settings, capsys):
+    settings.CATALOG_CACHE_BASE_DIR = tmpdir.strpath
+
+    expected = {
+        'id': 'foo', 'name': 'Content from Foo',
+        'url': 'https://foo.fr/catalog.yml'}
+
+    call_command(
+        'catalog', 'remotes', 'add', expected['id'], expected['name'],
+        expected['url'])
+
+    assert tmpdir.join('remotes').check(dir=True)
+    assert tmpdir.join('remotes', 'foo.yml').check(file=True)
+
+    with tmpdir.join('remotes', 'foo.yml').open('r') as f:
+        assert yaml.safe_load(f.read()) == expected
+
+    out, err = capsys.readouterr()
+    assert out.strip() == ''
+    assert err.strip() == ''
+
+
+def test_cannot_add_duplicate_remote(tmpdir, settings):
+    settings.CATALOG_CACHE_BASE_DIR = tmpdir.strpath
+
+    expected = {
+        'id': 'foo', 'name': 'Content from Foo',
+        'url': 'https://foo.fr/catalog.yml'}
+
+    call_command(
+        'catalog', 'remotes', 'add', expected['id'], expected['name'],
+        expected['url'])
+
+    assert tmpdir.join('remotes').check(dir=True)
+    assert tmpdir.join('remotes', 'foo.yml').check(file=True)
+
+    old_mtime = tmpdir.join('remotes', 'foo.yml').mtime()
+
+    with pytest.raises(CommandError):
+        call_command(
+            'catalog', 'remotes', 'add', expected['id'], expected['name'],
+            expected['url'])
+
+    assert tmpdir.join('remotes', 'foo.yml').mtime() == old_mtime
+
+
+def test_remove_remote(tmpdir, settings, capsys):
+    settings.CATALOG_CACHE_BASE_DIR = tmpdir.strpath
+
+    expected = {
+        'id': 'foo', 'name': 'Content from Foo',
+        'url': 'https://foo.fr/catalog.yml'}
+
+    call_command(
+        'catalog', 'remotes', 'add', expected['id'], expected['name'],
+        expected['url'])
+    call_command('catalog', 'remotes', 'remove', expected['id'])
+
+    assert tmpdir.join('remotes').check(dir=True)
+    assert tmpdir.join('remotes').listdir() == []
+
+    out, err = capsys.readouterr()
+    assert out.strip() == ''
+    assert err.strip() == ''
+
+
+def test_cannot_remove_unexisting_remote(tmpdir, settings):
+    settings.CATALOG_CACHE_BASE_DIR = tmpdir.strpath
+
+    with pytest.raises(CommandError):
+        call_command('catalog', 'remotes', 'remove', 'foo')
+
+
+def test_list_no_remotes(tmpdir, settings, capsys):
+    settings.CATALOG_CACHE_BASE_DIR = tmpdir.strpath
+
+    call_command('catalog', 'remotes', 'list')
+
+    out, err = capsys.readouterr()
+    assert out.strip() == ''
+    assert err.strip() == ''
+
+
+def test_add_then_list_multiple_remotes(tmpdir, settings, capsys):
+    settings.CATALOG_CACHE_BASE_DIR = tmpdir.strpath
+
+    expected = [
+        {
+            'id': 'foo', 'name': 'Content from Foo',
+            'url': 'https://foo.fr/catalog.yml',
+            },
+        {
+            'id': 'bar', 'name': 'Content from Bar',
+            'url': 'https://foo.fr/catalog.yml',
+            },
+        ]
+
+    for r in expected:
+        call_command(
+            'catalog', 'remotes', 'add', r['id'], r['name'], r['url'])
+
+    call_command('catalog', 'remotes', 'list')
+
+    out, err = capsys.readouterr()
+    assert out.strip('\n').split('\n') == [
+        "             [{0[id]}] {0[name]} : {0[url]}".format(r)
+        for r in sorted(expected, key=itemgetter('id'))
+        ]
+    assert err.strip() == ''
+
+
+def test_add_then_remove_then_list_remote(tmpdir, settings, capsys):
+    settings.CATALOG_CACHE_BASE_DIR = tmpdir.strpath
+
+    expected = {
+        'id': 'foo', 'name': 'Content from Foo',
+        'url': 'https://foo.fr/catalog.yml'}
+
+    call_command(
+        'catalog', 'remotes', 'add', expected['id'], expected['name'],
+        expected['url'])
+    call_command('catalog', 'remotes', 'remove', expected['id'])
+    call_command('catalog', 'remotes', 'list')
+
+    out, err = capsys.readouterr()
+    assert out.strip() == ''
+    assert err.strip() == ''
