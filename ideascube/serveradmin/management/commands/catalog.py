e605079117fe50f5df151e9b1b4496c65adba05e
Mathieu Bridon <bochecha@daitauha.fr>
2016-02-11 11:45:39 +0100

    catalog: Add package management to the cli
    This commit lets us list, install, remove and upgrade packages right
    from the Django command line tool.
    
    Now we have an actually useful command line package manager.

--- ideascube/serveradmin/management/commands/catalog.py
+++ ideascube/serveradmin/management/commands/catalog.py
@@ -2,7 +2,7 @@ import argparse
 
 from django.core.management.base import BaseCommand, CommandError
 
-from ideascube.serveradmin.catalog import Catalog
+from ideascube.serveradmin.catalog import Catalog, NoSuchPackage
 
 
 class Command(BaseCommand):
@@ -12,6 +12,49 @@ class Command(BaseCommand):
         subs = parser.add_subparsers(
             title='Commands', dest='cmd', metavar='',
             parser_class=argparse.ArgumentParser)
+
+        required_ids = argparse.ArgumentParser('required_ids', add_help=False)
+        required_ids.add_argument(
+            'ids', nargs='+', metavar='ID',
+            help='One or more package ids (or globs)')
+
+        optional_ids = argparse.ArgumentParser('optional_ids', add_help=False)
+        optional_ids.add_argument(
+            'ids', nargs='*', metavar='ID', default=['*'],
+            help='Optional package ids (or globs)')
+
+        # -- Manage content ---------------------------------------------------
+        list = subs.add_parser(
+            'list', parents=[optional_ids], help='List packages')
+        group = list.add_mutually_exclusive_group()
+        group.add_argument(
+            '--installed', action='store_const', dest='filter',
+            const='installed', help='Only list installed packages')
+        group.add_argument(
+            '--available', action='store_const', dest='filter',
+            const='available', help='Only list available packages')
+        group.add_argument(
+            '--upgradable', action='store_const', dest='filter',
+            const='upgradable',
+            help='Only list installed packages with updates available')
+        group.add_argument(
+            '--all', action='store_const', dest='filter', const='all',
+            help='List available and installed packages (default)')
+        list.set_defaults(filter='all', func=self.list_packages)
+
+        install = subs.add_parser(
+            'install', parents=[required_ids],
+            help='Install packages')
+        install.set_defaults(func=self.install_packages)
+
+        remove = subs.add_parser(
+            'remove', parents=[required_ids], help='Remove packages')
+        remove.set_defaults(func=self.remove_packages)
+
+        upgrade = subs.add_parser(
+            'upgrade', aliases=['update'],
+            parents=[optional_ids], help='Upgrade packages')
+        upgrade.set_defaults(func=self.upgrade_packages)
 
         # -- Manage local cache -----------------------------------------------
         cache = subs.add_parser('cache', help='Manage cache')
@@ -53,6 +96,58 @@ class Command(BaseCommand):
 
         self.catalog = Catalog()
         options['func'](options)
+
+    # -- Manage content -------------------------------------------------------
+    def list_packages(self, options):
+        fmt = ' {0.id:20}  {0.version:10}  {0.size:5}  {0.name}'
+
+        if options['filter'] in ('all', 'installed'):
+            pkgs = self.catalog.list_installed(options['ids'])
+
+            if pkgs:
+                print('Installed packages')
+
+                for pkg in pkgs:
+                    print(fmt.format(pkg))
+
+        if options['filter'] in ('all', 'available'):
+            pkgs = self.catalog.list_available(options['ids'])
+
+            if pkgs:
+                print('Available packages')
+
+                for pkg in pkgs:
+                    print(fmt.format(pkg))
+
+        if options['filter'] == 'upgradable':
+            pkgs = self.catalog.list_upgradable(options['ids'])
+
+            if pkgs:
+                print('Available updates')
+
+                for pkg in pkgs:
+                    print(fmt.format(pkg))
+
+    def install_packages(self, options):
+        try:
+            self.catalog.install_packages(options['ids'])
+
+        except NoSuchPackage as e:
+            raise CommandError('No such package: {}'.format(e))
+
+    def remove_packages(self, options):
+        try:
+            self.catalog.remove_packages(options['ids'])
+
+        except NoSuchPackage as e:
+            raise CommandError('No such package: {}'.format(e))
+
+    def upgrade_packages(self, options):
+        try:
+            self.catalog.upgrade_packages(options['ids'])
+
+        except NoSuchPackage as e:
+            raise CommandError('No such package: {}'.format(e))
 
     # -- Manage local cache ---------------------------------------------------
     def update_cache(self, options):
