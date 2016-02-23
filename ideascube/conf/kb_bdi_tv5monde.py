5ac528eff7c5cb49e329de720cbabc2ac89fc50c
fheslouin <florian.heslouin@zimdeo.fr>
2016-02-23 12:39:50 +0100

    Add conf file for TV5 Monde Burundi

---
+++ ideascube/conf/kb_bdi_tv5monde.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""KoomBook conf"""
+from .kb import *  # noqa
+
+LANGUAGE_CODE = 'fr'
+IDEASCUBE_NAME = 'TV5 Monde Burundi'
+HOME_CARDS = STAFF_HOME_CARDS + [
+    {
+        'id': 'blog',
+    },
+    {
+        'id': 'mediacenter',
+    },
+    {
+        'id': 'gutemberg',
+    },
+    {
+        'id': 'khanacademy',
+    },
+]
