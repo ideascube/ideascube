aab3ddec9fc0c88bf6fb96e1efd762a277cb48ad
fheslouin <florian.heslouin@zimdeo.fr>
2016-02-29 14:48:26 +0100

    Add conf file for KoomBook Kytabu

---
+++ ideascube/conf/kb_eth_kytabu.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""KoomBook conf"""
+from .kb import *  # noqa
+
+LANGUAGE_CODE = 'fr'
+IDEASCUBE_NAME = 'KYTABU'
+HOME_CARDS = STAFF_HOME_CARDS + [
+    {
+        'id': 'blog',
+    },
+    {
+        'id': 'mediacenter',
+    },
+    {
+        'id': 'gutenberg',
+    },
+    {
+        'id': 'khanacademy',
+    },
+]
