be6da25326c9678cc9f4107b61ec26b5fbf33a62
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2016-03-22 15:48:06 +0100

    Also test the link in the document view.

--- ideascube/mediacenter/tests/test_views.py
+++ ideascube/mediacenter/tests/test_views.py
@@ -108,15 +108,16 @@ def test_staff_detail_page_has_no_edit_delete_link_package_id(staffapp):
     document = DocumentFactory(package_id="foo")
     response = staffapp.get(reverse('mediacenter:document_detail',
                                     kwargs={'pk': document.pk}), status=200)
-    assert "Document installed by a package." in response.content.decode()
+    assert "Cannot edit or delete this document." in response.content.decode()
+    assert '<a href="/en/mediacenter/document/{}/edit/">Edit</a>'.format(document.pk) not in response.content.decode()
 
 
 def test_staff_detail_page_has_edit_delete_link_no_package_id(staffapp):
     document = DocumentFactory(package_id="")
     response = staffapp.get(reverse('mediacenter:document_detail',
                                     kwargs={'pk': document.pk}), status=200)
-    assert "Document installed by a package." not in response.content.decode()
-
+    assert "Cannot edit or delete this document." not in response.content.decode()
+    assert '<a href="/en/mediacenter/document/{}/edit/">Edit</a>'.format(document.pk) in response.content.decode()
 
 def test_can_create_document(staffapp):
     assert not Document.objects.count()
