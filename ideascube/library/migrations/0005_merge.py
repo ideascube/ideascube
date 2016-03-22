586c3746b5f079d152c8603c732be565409a2b68
Yohan Boniface <yb@enix.org>
2016-03-22 17:23:26 +0100

    Fix conflicting migrations

---
+++ ideascube/library/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('library', '0004_auto_20160310_1830'),
+        ('library', '0004_auto_20160322_1502'),
+    ]
+
+    operations = [
+    ]
