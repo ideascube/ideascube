fefd65ae03ca201f86c1d733bde3551cc880014c
Yohan Boniface <yb@enix.org>
2016-03-30 16:47:30 +0200

    We still use old cestpassorcier and ted kiwix packages on Burundi servers

--- ideascube/conf/idb_bdi.py
+++ ideascube/conf/idb_bdi.py
@@ -14,9 +14,9 @@ USER_FORM_FIELDS = (
 
 HOME_CARDS = HOME_CARDS + [
     {
-        'id': 'cpassorcier',
+        'id': 'cpassorcier.old',
     },
     {
-        'id': 'ted',
+        'id': 'ted.old',
     },
 ]
