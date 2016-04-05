b34315f4b4c77dfcc4bc83901ca8786af1a3f12a
fheslouin <florian.heslouin@zimdeo.fr>
2016-04-05 15:07:57 +0200

    Add more content

--- ideascube/conf/kb_gin_conakry.py
+++ ideascube/conf/kb_gin_conakry.py
@@ -1,14 +1,48 @@
 # -*- coding: utf-8 -*-
 """KoomBook conf"""
 from .kb import *  # noqa
+from django.utils.translation import ugettext_lazy as _
 
 LANGUAGE_CODE = 'fr'
-IDEASCUBE_NAME = 'CONAKRY'
+IDEASCUBE_NAME = 'Conakry'
 HOME_CARDS = STAFF_HOME_CARDS + [
+    {
+        'id': 'blog',
+    },
+    {
+        'id': 'mediacenter',
+    },
+    {
+        'id': 'bsfcampus',
+    },
     {
         'id': 'koombookedu',
     },
     {
-        'id': 'bsfcampus',
+        'id': 'wikipedia',
+        'languages': 'fr',
+    },
+    {
+        'id': 'cest-pas-sorcier',
+    },
+    {
+        'id': 'ted',
+        'sessions': [
+            ('tedxgeneva2014.fr', 'Geneva 2014'),
+            ('tedxlausanne2012.fr', 'Lausanne 2012'),
+            ('tedxlausanne2013.fr', 'Lausanne 2013'),
+            ('tedxlausanne2014.fr', 'Lausanne 2014'),
+        ]
+    },
+    {
+        'id': 'universcience',
+        'languages': ['fr']
+    },
+    {
+        'id': 'maps',
+        'maps': [
+            (_('World'), 'world.map'),
+            ('Burundi', 'burundi.map'),
+        ]
     },
 ]
