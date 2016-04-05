f7a53f7dc09e44fd40367c7c973372314a476912
fheslouin <florian.heslouin@zimdeo.fr>
2016-04-05 14:38:21 +0200

    Add conf file for Conakry devices

---
+++ ideascube/conf/kb_gin_conakry.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""KoomBook conf"""
+from .kb import *  # noqa
+
+LANGUAGE_CODE = 'fr'
+IDEASCUBE_NAME = 'CONAKRY'
+HOME_CARDS = STAFF_HOME_CARDS + [
+    {
+        'id': 'koombookedu',
+    },
+    {
+        'id': 'bsfcampus',
+    },
+]
