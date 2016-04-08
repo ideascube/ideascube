3e0efaa2b51b637c70a6694775a22cf80234b2ff
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2016-04-08 19:34:32 +0200

    Add a fixture to create (and configure) a new STORAGE_ROOT dir per test.

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import django_webtest
+import os
 
 from django.core.urlresolvers import reverse
 
@@ -14,6 +15,41 @@ def user():
 @pytest.fixture()
 def staffuser():
     return UserFactory(short_name="Hello", password='password', is_staff=True)
+
+
+@pytest.yield_fixture()
+def storage_dir(monkeypatch, tmpdir, settings):
+    """Return a new temporary storage dir.
+    Settings are also modified to point to the directory."""
+    STORAGE_ROOT = settings.STORAGE_ROOT
+
+    # Get the paths to changes and their original values.
+    original_paths = (k for k in dir(settings.wrapped_object)
+                      if k.isupper() and not k.startswith('__'))
+    original_paths = (k for k in original_paths
+                      if k.endswith('_ROOT') or k.endswith('_DIR'))
+    original_paths = {k: getattr(settings, k) for k in original_paths}
+    original_paths = {k: v for k, v in original_paths.items() if v}
+
+    # Update paths replacing STORAGE_ROOT by tmpdir.
+    for k, v in original_paths.items():
+        rel_path = os.path.relpath(v, STORAGE_ROOT)
+        new_temp_path = os.path.join(str(tmpdir), rel_path)
+        os.makedirs(new_temp_path, exist_ok=True)
+        setattr(settings, k, new_temp_path)
+
+    # Change also the location of the default_storage as this is cached
+    # and not updated when settings changed.
+    monkeypatch.setattr(
+        'django.core.files.storage.default_storage.location',
+        settings.MEDIA_ROOT
+    )
+    monkeypatch.setattr(
+        'django.core.files.storage.default_storage.base_location',
+        settings.MEDIA_ROOT
+    )
+
+    yield str(tmpdir)
 
 
 @pytest.yield_fixture()
