75f17baeb2db206cd5970e9b98b01079a763fd38
Mathieu Bridon <bochecha@daitauha.fr>
2016-07-13 12:21:40 +0200

    Work around test brokenness
    Both our databases are supposed to be in memory when running the tests.
    
    Unfortunately, the transient ones is on the file system, under
    STORAGE_ROOT in two instances at the beginning of the tests:
    
    * when running the migrations
    * when firing the post_migrate signal in ideascube.search.apps
    
    This wouldn't have been a problem back when we were using the dev
    config as a fallback, since that config created STORAGE_ROOT.
    
    However, we now fallback to the base config, which doesn't try creating
    its STORAGE_ROOT.
    
    As a result, the tests are failing, because the transient db couldn't
    be created, because its parent folder didn't exist.
    
    Explicitly setting the TEST_NAME in the config file to ':memory:' doesn't
    change anything.
    
    But somehow, doing it in pytest_configure does the trick.

--- conftest.py
+++ conftest.py
@@ -89,3 +89,20 @@ def testdatadir(request):
 
     assert datadir.check(dir=True)
     return datadir
+
+
+def pytest_configure(config):
+    from django.conf import settings
+
+    # This is already supposed to be the case by default, and we even tried
+    # setting it explicitly anyway.
+    #
+    # But somehow, at the very beginning of the test suite (when running the
+    # migrations or when the post_migrate signal is fired), the transient
+    # database is on the filesystem (the value of NAME).
+    #
+    # We can't figure out why that is, it might be a bug in pytest-django, or
+    # worse in django itself.
+    #
+    # Somehow the default database is always in memory, though.
+    settings.DATABASES['transient']['TEST_NAME'] = ':memory:'
