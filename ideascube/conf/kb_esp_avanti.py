7c6dfd810e0567a0aa24515f8d57a5d609e38e95
fheslouin <florian.heslouin@zimdeo.fr>
2016-07-13 12:34:36 +0200

    Add conf file for Avanti KoomBook

---
+++ ideascube/conf/kb_esp_avanti.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""KoomBook conf"""
+from .kb import *  # noqa
+
+LANGUAGE_CODE = 'es'
+IDEASCUBE_NAME = 'Avanti'
+HOME_CARDS = STAFF_HOME_CARDS + [
+    {
+        'id': 'blog',
+    },
+    {
+        'id': 'mediacenter',
+    },
+    {
+        'id': 'gutenberg',
+        'lang': 'es',
+    },
+    {
+        'id': 'wikipedia',
+        'languages': ['es']
+    },
+    {
+        'id': 'khanacademy',
+    },
+]
