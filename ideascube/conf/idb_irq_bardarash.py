a19131c652f7294ad2b761cbe1506f90be25c1f4
ToM <thomas.faucher@bibliosansfrontieres.org>
2016-07-19 10:58:52 +0200

    Apply misc fixes from peers review

--- ideascube/conf/idb_irq_bardarash.py
+++ ideascube/conf/idb_irq_bardarash.py
@@ -4,14 +4,7 @@ from .azraq import *  # noqa
 from django.utils.translation import ugettext_lazy as _
 
 IDEASCUBE_NAME = u"Bardarash"
-USER_FORM_FIELDS = (
-    ('Ideasbox', ['serial', 'box_awareness']),
-    (_('Personal informations'), ['refugee_id', 'short_name', 'full_name', 'latin_name', 'birth_year', 'gender']),  # noqa
-    (_('Family'), ['marital_status', 'family_status', 'children_under_12', 'children_under_18', 'children_above_18']),  # noqa
-    (_('Origin'), ['country', 'city', 'country_of_origin_occupation', 'school_level', 'is_sent_to_school']),  # noqa
-    (_('Language skills'), ['ar_level', 'en_level']),
-    (_('National residents'), ['id_card_number']),
-)
+
 HOME_CARDS = STAFF_HOME_CARDS + [
     {
         'id': 'blog',
@@ -27,10 +20,10 @@ HOME_CARDS = STAFF_HOME_CARDS + [
     },
     {
         'id': 'wikipedia',
-        'languages': ['en','ar']
+        'languages': ['en', 'ar']
     },
     {
         'id': 'gutenberg',
-        'languages': ['en','ar']
+        'lang': 'mul',
     },
 ]
