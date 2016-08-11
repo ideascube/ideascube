36ab17e017ae59f2adbbea7a078b43b2dd3852f2
ToM <thomas.faucher@bibliosansfrontieres.org>
2016-08-11 11:59:18 +0200

    Remove useless comments

--- ideascube/conf/idb_grc_malakasa.py
+++ ideascube/conf/idb_grc_malakasa.py
@@ -1,14 +1,10 @@
-# -*- coding: utf-8 -*-
 """Ideasbox Malakasa, Grece"""
 from .idb import *  # noqa
 from django.utils.translation import ugettext_lazy as _
 
-#IDEASCUBE_NAME = u"Malakasa"
-#IDEASCUBE_PLACE_NAME = _("camp")
 COUNTRIES_FIRST = ['AF', 'GR', 'IR', 'PK', 'SY']
 TIME_ZONE = 'Europe/Athens'
 LANGUAGE_CODE = 'en'
-#LOAN_DURATION = 14
 USER_FORM_FIELDS = (
     (_('Personal informations'), ['serial', 'short_name', 'full_name', 'latin_name', 'birth_year', 'gender']),  # noqa
     (_('In the town'), ['current_occupation', 'school_level', 'phone', 'email']),  # noqa
