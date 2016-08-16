13950d7a97f3abac7f5d374486f11b4f833ee875
ToM <thomas.faucher@bibliosansfrontieres.org>
2016-08-16 14:59:36 +0200

    Bwagiriza reinstall. Adapt the config to new standards.

--- ideascube/conf/idb_bdi_bwagiriza.py
+++ ideascube/conf/idb_bdi_bwagiriza.py
@@ -1,3 +1,46 @@
 """Bwagiriza box in Burundi"""
 from .idb_bdi import *  # noqa
-IDEASCUBE_NAME = "Bwagiriza"
+
+HOME_CARDS = STAFF_HOME_CARDS + [
+    {
+        'id': 'blog',
+    },
+    {
+        'id': 'library',
+    },
+    {
+        'id': 'mediacenter',
+    },
+    {
+        'id': 'wikipedia',
+        'languages': ['fr','rn','sw']
+    },
+    {
+        'id': 'vikidia',
+        'languages': ['fr']
+    },
+    {
+        'id': 'khanacademy',
+    },
+    {
+        'id': 'gutenberg',
+        'lang': 'mul',
+    },
+    {
+        'id': 'ted',
+        'sessions': [
+            ('tedxgeneva2014.fr', 'Geneva 2014'),
+            ('tedxlausanne2012.fr', 'Lausanne 2012'),
+            ('tedxlausanne2013.fr', 'Lausanne 2013'),
+            ('tedxlausanne2014.fr', 'Lausanne 2014'),
+            ('tedxlausannechange2013.fr', 'Lausanne Exchange 2013'),
+        ]
+    },
+    {
+        'id': 'maps',
+        'maps': [
+            ('World', 'world.map'),
+            ('Burundi', 'burundi.map'),
+        ]
+    }
+]
