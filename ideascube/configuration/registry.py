c227a0df6b03c0218517ea632c24de8dddff8380
Mathieu Bridon <bochecha@daitauha.fr>
2016-08-18 13:53:00 +0200

    configuration: Add a registry of all known configs
    This will be useful to tighten a bit the API and avoid mistakes.
    
    It will also be very useful when we want to implement a command-line
    tool to manage the configuration.
    
    For now, the registry only contains the namespaces and keys we already
    use as settings, more will come eventually.
    
    Relates to #441

---
+++ ideascube/configuration/registry.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+
+# Please try and keep this stored in alphabetical order
+REGISTRY = {
+    # 'namespace': {
+    #     'key': {
+    #     },
+    # },
+    'server': {
+        'site-name': {
+        },
+    },
+}
