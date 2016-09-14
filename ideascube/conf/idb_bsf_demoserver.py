b552b6db1b8e965dc371132f35547d7a6ad371b5
ToM <thomas.faucher@bibliosansfrontieres.org>
2016-09-14 14:13:53 +0200

    Update the demoserver address.

--- ideascube/conf/idb_bsf_demoserver.py
+++ ideascube/conf/idb_bsf_demoserver.py
@@ -2,8 +2,8 @@
 """Configuration for a demo server hosted here, at BSF Headquarters"""
 from .idb import *  # noqa
 
-ALLOWED_HOSTS = ['.demo.ideascube.ideas-box.fr', '.ideasbox.lan.', 'localhost']
-DOMAIN = 'demo.ideascube.ideas-box.fr'
+ALLOWED_HOSTS = ['.demo.ideascube.org', '.ideasbox.lan.', 'localhost']
+DOMAIN = 'demo.ideascube.org'
 
 LANGUAGE_CODE = 'es'
 HOME_CARDS = STAFF_HOME_CARDS + [
