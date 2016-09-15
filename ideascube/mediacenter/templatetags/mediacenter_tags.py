8c536774cfe40783350f6f569eb6580e79499600
Mathieu Bridon <bochecha@daitauha.fr>
2016-09-15 11:31:15 +0200

    mediacenter: Drop the preview_url template filter
    This is a bit sad as it means having more if/elif/else logic in the
    templates.
    
    However, this will be necessary for a future commit.
    
    Relates to #468

--- ideascube/mediacenter/templatetags/mediacenter_tags.py
+++ ideascube/mediacenter/templatetags/mediacenter_tags.py
@@ -4,13 +4,6 @@ from django.contrib.staticfiles.templatetags.staticfiles import static
 register = template.Library()
 
 
-@register.filter()
-def preview_url(inst):
-    """Do its best to return a preview URL that makes sense according to
-    document instance given as parameter."""
-    if inst.preview:
-        return inst.preview.url
-    elif inst.kind == inst.IMAGE:
-        return inst.original.url
-    else:
-        return static('mediacenter/default_{}.png'.format(inst.kind))
+@register.filter
+def default_preview_url(inst):
+    return static('mediacenter/default_{}.png'.format(inst.kind))
