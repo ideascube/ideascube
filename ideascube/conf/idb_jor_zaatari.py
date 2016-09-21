9a80f30acc2b57c37726d765977ff471b0033e1a
ToM <thomas.faucher@bibliosansfrontieres.org>
2016-09-21 17:28:35 +0200

    Add configuration for Zaatari, Jordan.

---
+++ ideascube/conf/idb_jor_zaatari.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""Ideaxbox for Zaatari, Jordan"""
+from .idb import *  # noqa
+from django.utils.translation import ugettext_lazy as _
+
+IDEASCUBE_NAME = u"Zaatari"
+IDEASCUBE_PLACE_NAME = _("city")
+COUNTRIES_FIRST = ['SY', 'JO']
+TIME_ZONE = 'Asia/Amman'
+LANGUAGE_CODE = 'ar'
+LOAN_DURATION = 14
+MONITORING_ENTRY_EXPORT_FIELDS = ['serial', 'user_id', 'birth_year', 'gender']
+USER_FORM_FIELDS = (
+    ('Ideasbox', ['serial', 'box_awareness']),
+    (_('Personal informations'), ['short_name', 'full_name', 'birth_year', 'gender', 'id_card_number']),  # noqa
+    (_('Family'), ['marital_status', 'family_status', 'children_under_12', 'children_under_18', 'children_above_18']),  # noqa
+    (_('In the town'), ['current_occupation', 'school_level']),
+    (_('Language skills'), ['en_level']),
+)
+HOME_CARDS = STAFF_HOME_CARDS + [
+    {
+        'id': 'blog',
+    },
+    {
+        'id': 'library',
+    },
+    {
+        'id': 'mediacenter',
+    },
+    {
+        'id': 'khanacademy',
+    },
+    {
+        'id': 'wikipedia',
+        'languages': ['en','ar']
+    },
+    {
+        'id': 'wiktionary',
+        'languages': ['en', 'ar']
+    },
+    {
+        'id': 'wikiversity',
+        'languages': ['en', 'ar']
+    },
+    {
+        'id': 'wikibooks',
+        'languages': ['en', 'ar']
+    },
+    {
+        'id': 'wikisource',
+        'languages': ['en', 'ar']
+    },
+    {
+        'id': 'wikiquote',
+        'languages': ['en', 'ar']
+    },
+    {
+        'id': 'ted',
+        'sessions': [
+            ('tedbusiness.en', 'Business'),
+            ('teddesign.en', 'Design'),
+            ('tedentertainment.en', 'Entertainment'),
+            ('tedglobalissues.en', 'Global Issues'),
+            ('tedscience.en', 'Science'),
+            ('tedtechnology.en', 'Technology'),
+            ('ted-ed', 'Education'),
+            ('tedmed.en', 'Medical'),
+        ]
+    },
+    {
+        'id': 'gutenberg',
+        'languages': ['en', 'fr']
+    },
+    {
+        'id': 'crashcourse',
+        'languages': ['en']
+    },
+    {
+        'id': 'vikidia',
+        'languages': ['en']
+    },
+]
