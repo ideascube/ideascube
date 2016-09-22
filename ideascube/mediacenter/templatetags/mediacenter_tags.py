6b480b6ceae05367d74c3ceb13fa0732a7e504fd
Mathieu Bridon <bochecha@daitauha.fr>
2016-09-22 09:58:33 +0200

    We already use the static tag in the templates
    I introduced this error in 8c536774cfe40783350f6f569eb6580e79499600.
    
    Fixes #492

--- ideascube/mediacenter/templatetags/mediacenter_tags.py
+++ ideascube/mediacenter/templatetags/mediacenter_tags.py
@@ -1,9 +1,8 @@
 from django import template
-from django.contrib.staticfiles.templatetags.staticfiles import static
 
 register = template.Library()
 
 
 @register.filter
 def default_preview_url(inst):
-    return static('mediacenter/default_{}.png'.format(inst.kind))
+    return 'mediacenter/default_{}.png'.format(inst.kind)
