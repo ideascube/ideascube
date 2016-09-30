6b4a460686429fa478777ae0e5f859b5ab03a6a5
Yohan Boniface <yb@enix.org>
2016-09-30 16:17:55 +0200

    Fix test_settings checking for wrong extension

--- ideascube/tests/test_settings.py
+++ ideascube/tests/test_settings.py
@@ -6,11 +6,13 @@ import pytest
 def test_all_settings(request):
     confdir = request.fspath.dirpath().dirpath().join('conf')
     assert confdir.check(dir=True)
+    count = 0
 
     for path in confdir.listdir():
-        if path.ext != 'py':
+        if path.ext != '.py':
             continue
 
+        count += 1
         module = path.purebasename
         module = '.conf.%s' % module
 
@@ -23,3 +25,6 @@ def test_all_settings(request):
             pytest.fail(str(e))
 
         assert isinstance(getattr(settings, 'IDEASCUBE_NAME', ''), str)
+
+    # Make sure we have actually tested some files.
+    assert count
