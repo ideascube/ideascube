70ec236c88a8854c828c363b22a0bf24151265bb
Yohan Boniface <yb@enix.org>
2016-09-30 15:41:09 +0200

    Add an idb_fra default file conf

--- ideascube/conf/idb_fra_cultura.py
+++ ideascube/conf/idb_fra_cultura.py
@@ -1,14 +1,8 @@
 """Ideaxbox Cultura, France"""
-from .idb import *  # noqa
+from .idb_fra import *  # noqa
 from django.utils.translation import ugettext_lazy as _
 
 IDEASCUBE_NAME = u"Cultura"
-IDEASCUBE_PLACE_NAME = _("city")
-COUNTRIES_FIRST = ['FR']
-TIME_ZONE = None
-LANGUAGE_CODE = 'fr'
-LOAN_DURATION = 14
-MONITORING_ENTRY_EXPORT_FIELDS = ['serial', 'user_id', 'birth_year', 'gender']
 USER_FORM_FIELDS = (
     (_('Personal informations'), ['serial', 'short_name', 'full_name', 'latin_name', 'birth_year', 'gender']),  # noqa
 )
