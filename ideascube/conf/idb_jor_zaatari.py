d6f6d41665f58e68833b57d8b0d04d113f2c86a9
Yohan Boniface <yb@enix.org>
2016-09-30 15:44:50 +0200

    Make zaatari import from azraq

--- ideascube/conf/idb_jor_zaatari.py
+++ ideascube/conf/idb_jor_zaatari.py
@@ -1,17 +1,4 @@
 """Ideaxbox for Zaatari, Jordan"""
-from .idb import *  # noqa
-from django.utils.translation import ugettext_lazy as _
+from .idb_jor_azraq import *  # noqa
 
-IDEASCUBE_PLACE_NAME = _("city")
-COUNTRIES_FIRST = ['SY', 'JO']
-TIME_ZONE = 'Asia/Amman'
-LANGUAGE_CODE = 'ar'
-LOAN_DURATION = 14
-MONITORING_ENTRY_EXPORT_FIELDS = ['serial', 'user_id', 'birth_year', 'gender']
-USER_FORM_FIELDS = (
-    ('Ideasbox', ['serial', 'box_awareness']),
-    (_('Personal informations'), ['short_name', 'full_name', 'birth_year', 'gender', 'id_card_number']),  # noqa
-    (_('Family'), ['marital_status', 'family_status', 'children_under_12', 'children_under_18', 'children_above_18']),  # noqa
-    (_('In the town'), ['current_occupation', 'school_level']),
-    (_('Language skills'), ['en_level']),
-)
+ENTRY_ACTIVITY_CHOICES = []
