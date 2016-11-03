41be123ca0eed03b32826bf35f2d90dc6f03f838
Matthieu Gautier <matthieu.gautier@mgautier.fr>
2016-11-03 15:10:25 +0100

    Add a helper to test migrations.
    Py.test/Django initialise first the db by applying all migrations and
    then run all test using the setup db as base.
    This helper :
     - reverts the migration from the base to `migrate_from`,
     - call the setUpDataBeforeMigration(),
     - migrate to `migrate_to`,
     - launch the test.
    
    This implies that **all** migrations after `migrate_from` must be
    revertible !
    
    If tests are run without migrations (option --nomigrations), migration tests
    will be skipped.

---
+++ ideascube/tests/helpers.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+def migration_test(migrate_from, migrate_to):
+    no_migration_skip = pytest.mark.skipif(pytest.config.getoption("--nomigrations"),
+                                           reason="skipping due to --nomigrations"
+                                          )
+    parametrize_migration = pytest.mark.parametrize('migration',
+                         [(migrate_from,
+                           migrate_to
+                         )],
+                         indirect=['migration'])
+    def wrapper(function):
+        f = parametrize_migration(function)
+        f = no_migration_skip(f)
+        return f
+    return wrapper
