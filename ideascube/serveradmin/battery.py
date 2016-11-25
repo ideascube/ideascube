b8e96b8de936d5d72ed979cba84dd63f4f24432a
Mathieu Bridon <bochecha@daitauha.fr>
2016-11-25 18:28:49 +0100

    Workaround the bad Koombook battery driver
    This is pretty ugly, but the Koombook battery driver doesn't respect the
    Linux convention for reporting the power status to sysfs.
    
    As a result, batinfo doesn't work with it.
    
    Since this is quite specific, I don't think it makes sense to push that
    upstream to batinfo, so I opted for a workaround in our code.
    
    Fixes #446

--- ideascube/serveradmin/battery.py
+++ ideascube/serveradmin/battery.py
@@ -1,11 +1,29 @@
 import batinfo
 
 
+class Lime2Battery(batinfo.Battery):
+    @property
+    def status(self):
+        if self.charging == 0:
+            return 'Discharging'
+
+        elif self.capacity < 100:
+            return 'Charging'
+
+        else:
+            return 'Full'
+
+
 def get_batteries():
     batteries = batinfo.batteries()
 
     if batteries:
         return batteries.stat
 
-    else:
+    try:
+        # We might be running on a Lime2 Koombook
+        # https://github.com/ideascube/ideascube/issues/446#issuecomment-244143565
+        return [Lime2Battery(path='/sys/power/axp_pmu', name='battery')]
+
+    except FileNotFoundError:
         return []
