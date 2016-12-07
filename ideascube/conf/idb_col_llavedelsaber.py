b89716c4e7ba69c36a04bca00da20cfa8bb6a5e7
ToM <thomas.faucher@bibliosansfrontieres.org>
2016-12-07 16:53:06 +0100

    Add a custom 'extra' field to idb_col_llavadelsaber

--- ideascube/conf/idb_col_llavedelsaber.py
+++ ideascube/conf/idb_col_llavedelsaber.py
@@ -4,3 +4,9 @@ from .idb import *  # noqa
 LANGUAGE_CODE = 'es'
 DOMAIN = 'bibliotecamovil.lan'
 ALLOWED_HOSTS = ['.bibliotecamovil.lan', 'localhost']
+
+USER_FORM_FIELDS = USER_FORM_FIELDS + (
+        (_('Personal informations'), ['extra']),
+)
+
+USER_EXTRA_FIELD_LABEL = 'Etnicidad'
