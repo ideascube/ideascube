bc55a8d67d6cdd05c6de41a9d7d543fa6105002e
Mathieu Bridon <bochecha@daitauha.fr>
2017-03-23 15:28:42 +0100

    Move some code around
    We're going to have more cards-related things very soon, so let's move
    the existing one to its own module now.

---
+++ ideascube/tests/test_cards.py
@@ -0,0 +1,43 @@
+def test_build_package_card_info_must_not_fail_for_no_package(systemuser):
+    from ideascube.configuration import set_config
+    from ideascube.views import build_package_card_info
+    set_config('home-page', 'displayed-package-ids', ['test.package1'], systemuser)
+    assert build_package_card_info() == []
+
+
+def test_build_package_card_info(systemuser, catalog):
+    from ideascube.configuration import set_config
+    from ideascube.views import build_package_card_info
+    from ideascube.serveradmin.catalog import ZippedZim, ZippedMedias
+    catalog.add_mocked_package(ZippedZim('test.package1.fr', {
+        'name':'Test package1',
+        'description':'Test package1 description',
+        'language':'fr',
+        'staff_only':False}))
+    catalog.add_mocked_package(ZippedMedias('test.package2.fr', {
+        'name':'Test package2',
+        'description':'Test package2 description',
+        'language':'fr',
+        'staff_only':False}))
+    set_config('home-page', 'displayed-package-ids', ['test.package1.fr', 'test.package2.fr'], systemuser)
+
+    assert build_package_card_info() == [{
+        'package_id' : 'test.package1.fr',
+        'name'       : 'Test package1',
+        'description': 'Test package1 description',
+        'lang'       : 'fr',
+        'is_staff'   : False,
+        'id'         : 'kiwix',
+        'css_class'  : 'test.package1',
+        'theme'      : 'learn'
+    },
+    {
+        'package_id' : 'test.package2.fr',
+        'name'       : 'Test package2',
+        'description': 'Test package2 description',
+        'lang'       : 'fr',
+        'is_staff'   : False,
+        'id'         : 'media-package',
+        'css_class'  : None,
+        'theme'      : None
+    }]
