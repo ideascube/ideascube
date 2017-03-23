23d7fc47d995248b386adc9268a16555be6d5438
Matthieu Gautier <mgautier@kymeria.fr>
2017-03-23 17:05:28 +0100

    0.23.0

--- ideascube/__init__.py
+++ ideascube/__init__.py
@@ -4,6 +4,6 @@ if sys.version_info.major != 3:
     sys.exit('ERROR: Ideascube only works with Python 3')
 
 
-VERSION = (0, 22, 0)
+VERSION = (0, 23, 0)
 
 __version__ = ".".join(map(str, VERSION))
