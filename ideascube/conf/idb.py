9c641ed30e54b05195ca1bbd693117f21f14c313
Mathieu Bridon <bochecha@daitauha.fr>
2017-03-24 16:50:27 +0100

    conf: Remove some unnecessary HOME_CARDS options
    There is no need to redefine a setting, if we're setting it to the value
    inherited from ideascube.conf.base.

--- ideascube/conf/idb.py
+++ ideascube/conf/idb.py
@@ -3,18 +3,3 @@ from .base import *  # pragma: no flakes
 
 ALLOWED_HOSTS = ['.ideasbox.lan', 'localhost']
 DOMAIN = 'ideasbox.lan'
-
-HOME_CARDS = STAFF_HOME_CARDS + [  # pragma: no flakes
-    {
-        'id': 'blog',
-    },
-    {
-        'id': 'mediacenter',
-    },
-    {
-        'id': 'library',
-    },
-    {
-        'id': 'khanacademy',
-    }
-]
