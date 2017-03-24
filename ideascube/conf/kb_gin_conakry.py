8562ae092edc565c8a276049e00f813bddc69598
Mathieu Bridon <bochecha@daitauha.fr>
2017-03-24 16:23:18 +0100

    conf: Simplify a bit
    This isn't much, just a more idiomatic (and with less typing!) way to do
    the same thing.

--- ideascube/conf/kb_gin_conakry.py
+++ ideascube/conf/kb_gin_conakry.py
@@ -3,7 +3,7 @@ from .kb_bsfcampus import *  # pragma: no flakes
 
 LANGUAGE_CODE = 'fr'
 IDEASCUBE_NAME = 'Conakry'
-HOME_CARDS = HOME_CARDS + [  # pragma: no flakes
+HOME_CARDS += [  # pragma: no flakes
     {
         'id': 'koombookedu',
     },
