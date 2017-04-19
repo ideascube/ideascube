e5cfe5f2f916aef6c65d3c0ee9031c6191a08edf
Mathieu Bridon <bochecha@daitauha.fr>
2017-04-19 13:32:19 +0200

    blog: Remove the author_text field
    Feedback from the field indicates that nobody understands how this is
    supposed to be used.
    
    We need to agree this was an interesting, but inconclusive experiment,
    and just remove it.
    
    Fixes #758

--- ideascube/blog/tests/test_models.py
+++ ideascube/blog/tests/test_models.py
@@ -8,18 +8,6 @@ from .factories import ContentFactory
 pytestmark = pytest.mark.django_db
 
 
-def test_get_author_display_should_return_author_text(published):
-    text = 'An author'
-    published.author_text = text
-    assert published.get_author_display() == text
-
-
-def test_get_author_display_should_return_author_if_no_author_text(user):
-    content = ContentFactory(author=user)
-    content.author_text = ''
-    assert content.get_author_display() == str(user)
-
-
 def test_cannot_delete_user_if_author_of_blog_content(user):
     content = ContentFactory(author=user)
     with pytest.raises(IntegrityError):
