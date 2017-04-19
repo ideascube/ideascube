b7dec575bc063f966e52f9dfffd3cfc394ee365b
Mathieu Bridon <bochecha@daitauha.fr>
2017-04-19 13:40:39 +0200

    blog: Hide the author field
    Feedback from the field indicates that people are confused than this.
    
    Since we can automatically guess the author of the post as the logged in
    user writing it, let's just do that.
    
    Relates to #344

--- ideascube/blog/tests/test_views.py
+++ ideascube/blog/tests/test_views.py
@@ -229,7 +229,6 @@ def test_can_create_content_without_image(staffapp):
     form['title'] = 'my content title'
     form['summary'] = 'my content summary'
     form['text'] = 'my content text'
-    form['author'] = staffapp.user.pk
     form['published_at'] = '2014-12-10'
     form.submit().follow()
     assert Content.objects.count()
@@ -304,7 +303,6 @@ def test_can_create_content_with_tags(staffapp):
     form['title'] = 'my content title'
     form['summary'] = 'my content summary'
     form['text'] = 'my content text'
-    form['author'] = staffapp.user.pk
     form['published_at'] = '2014-12-10'
     form['tags'] = 'tag1, tag2'
     form.submit().follow()
