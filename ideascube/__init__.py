4c58f31e995cdb9c9d5d8bee1fd28ffc693ebe79
Mathieu Bridon <bochecha@daitauha.fr>
2017-05-05 10:54:19 +0200

    0.26.1

--- ideascube/__init__.py
+++ ideascube/__init__.py
@@ -4,6 +4,6 @@ if sys.version_info.major != 3:
     sys.exit('ERROR: Ideascube only works with Python 3')
 
 
-VERSION = (0, 26, 0)
+VERSION = (0, 26, 1)
 
 __version__ = ".".join(map(str, VERSION))
