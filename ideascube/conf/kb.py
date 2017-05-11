cab1999e9bda3698ef2e7341f07bbe754e7f1d62
Mathieu Bridon <bochecha@daitauha.fr>
2017-05-11 13:30:54 +0200

    home: Simplify the settings some more
    We have several setting options to control the cards visible on the home
    page.
    
    One of them, EXTRA_APP_CARDS control the visibility of some extra
    applications which can be installed along with Ideascube.
    
    It basically only lists card identifiers, but does so in a format which
    is more complex than it needs to be, for historical reasons.
    
    This commit simplifies the setting files, moving the complexity to the
    code.

--- ideascube/conf/kb.py
+++ ideascube/conf/kb.py
@@ -11,9 +11,4 @@ STAFF_HOME_CARDS = [c for c in STAFF_HOME_CARDS  # pragma: no flakes
                     if c['url'] in ['user_list', 'server:settings']]
 
 BUILTIN_APP_CARDS = ['blog', 'mediacenter']
-
-EXTRA_APP_CARDS = [
-    {
-        'id': 'khanacademy',
-    },
-]
+EXTRA_APP_CARDS = ['khanacademy']
--- ideascube/conf/kb_cdf_fra.py
+++ ideascube/conf/kb_cdf_fra.py
@@ -4,8 +4,4 @@ from .kb import *  # pragma: no flakes
 LANGUAGE_CODE = 'fr'
 IDEASCUBE_NAME = 'Coeur de Forêt'
 
-EXTRA_APP_CARDS = [
-    {
-        'id': 'appinventor',
-    },
-]
+EXTRA_APP_CARDS = ['appinventor']
