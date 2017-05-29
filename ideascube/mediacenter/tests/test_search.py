eebc1ac9910f78b1eb783b3623436eb4db0d2e52
Matthieu Gautier <mgautier@kymeria.fr>
2017-05-29 18:38:17 +0200

    Use one search model per model instead of one search model for all.
    We want to decouple the django applications. For this we need to not
    store all the index object in the same table.
    
    This "big" commit does:
    
     - Automatically create a Search model per searchable model.
       This is done in the `SearchMixin` metaclass as all searchable model
       inherit from `SearchMixin`.
     - The `Search` model is now abstract.
     - As the `Search` model is abstract, we need to use the specific Search
       model instead of the abstract one.

--- ideascube/mediacenter/tests/test_search.py
+++ ideascube/mediacenter/tests/test_search.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from ideascube.search.models import Search
-
 from ..models import Document
 
 pytestmark = pytest.mark.django_db
@@ -10,17 +8,17 @@ pytestmark = pytest.mark.django_db
 
 @pytest.mark.usefixtures('cleansearch')
 def test_document_are_indexed(video):
-    assert Search.objects.count() == 1
-    assert len(list(Search.search(public=True))) == 1
+    assert Document.SearchModel.objects.count() == 1
+    assert len(list(Document.SearchModel.search(public=True))) == 1
     assert Document.objects.search("Ikinyugunyugu").count() == 0
     video.title = "Ikinyugunyugu"
     video.save()
-    assert Search.objects.count() == 1
+    assert Document.SearchModel.objects.count() == 1
     assert Document.objects.search("Ikinyugunyugu").count() == 1
 
 
 @pytest.mark.usefixtures('cleansearch')
 def test_hard_delete_is_deindexed(video):
-    assert Search.objects.count() == 1
+    assert Document.SearchModel.objects.count() == 1
     video.delete()
-    assert Search.objects.count() == 0
+    assert Document.SearchModel.objects.count() == 0
