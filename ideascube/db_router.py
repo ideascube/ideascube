81a3386df04cb38bf11bc882e9a6b36d5cc14948
Matthieu Gautier <mgautier@kymeria.fr>
2017-05-29 16:26:25 +0200

    Use a intermediate get_database function to route the database operations.
    This is mainly to simplifies future changes.

--- ideascube/db_router.py
+++ ideascube/db_router.py
@@ -4,6 +4,10 @@ _database_to_use = {
 }
 
 
+def get_database(app_label, model_name, default=None):
+    return _database_to_use.get((app_label, model_name), default)
+
+
 class DatabaseRouter(object):
     """
     A Database router.
@@ -15,16 +19,14 @@ class DatabaseRouter(object):
         Return the database from which this model can be read,
         or None to use the default database.
         """
-        app_model = model._meta.app_label, model._meta.model_name
-        return _database_to_use.get(app_model)
+        return get_database(model._meta.app_label, model._meta.model_name)
 
     def db_for_write(self, model, **hints):
         """
         Return database for which this model can be write,
         or None to use the default database.
         """
-        app_model = model._meta.app_label, model._meta.model_name
-        return _database_to_use.get(app_model)
+        return get_database(model._meta.app_label, model._meta.model_name)
 
     def allow_relation(self, obj1, obj2, **hints):
         """Return whether a relation is allowed between these two models
@@ -37,17 +39,14 @@ class DatabaseRouter(object):
         # However, there is no JOIN relation between two tables of two
         # different db for now. So it seems safe to me to simply allow relation
         # if the tables are in the same db.
-
-        app_model1 = obj1._meta.app_label, obj1._meta.model_name
-        app_model2 = obj2._meta.app_label, obj2._meta.model_name
-        return _database_to_use.get(app_model1) \
-            == _database_to_use.get(app_model2)
+        return get_database(obj1._meta.app_label, obj1._meta.model_name) \
+            == get_database(obj2._meta.app_label, obj2._meta.model_name)
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """
         Allow migration only if the migration is applying to the right db.
         """
-        db_to_use = _database_to_use.get((app_label, model_name), 'default')
+        db_to_use = get_database(app_label, model_name, default='default')
         if db_to_use != db:
             return False
         using = hints.get('using')
