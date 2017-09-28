2d849c80a7d10d4e86c23fb768e6cae3e8c0c720
Mathieu Bridon <bochecha@daitauha.fr>
2017-09-28 11:35:52 +0200

    Make the lang mandatory when importing medias
    Relates to #760

--- ideascube/mediacenter/tests/test_import_medias.py
+++ ideascube/mediacenter/tests/test_import_medias.py
@@ -22,31 +22,34 @@ def teardown_function(function):
 
 def test_should_import_medias():
     assert not Document.objects.count()
-    metadata = ('kind,title,summary,credits,path\n'
-                'video,my video,my video summary,BSF,a-video.mp4\n'
-                'pdf,my doc,my doc summary,BSF,a-pdf.pdf\n'
-                'image,my image,my image summary,BSF,an-image.jpg\n'
-                'image,no summary,,BSF,an-image.jpg\n'
-                'image,no credits,my summary,,an-image.jpg\n')
+    metadata = ('kind,title,summary,credits,path,lang\n'
+                'video,my video,my video summary,BSF,a-video.mp4,ar\n'
+                'pdf,my doc,my doc summary,BSF,a-pdf.pdf,en\n'
+                'image,my image,my image summary,BSF,an-image.jpg,es\n'
+                'image,no summary,,BSF,an-image.jpg,fr\n'
+                'image,no credits,my summary,,an-image.jpg,ku\n')
     write_metadata(metadata)
     call_command('import_medias', CSV_PATH)
     assert Document.objects.count() == 5
     video = Document.objects.get(title='my video')
     assert video.summary == 'my video summary'
     assert video.kind == Document.VIDEO
+    assert video.lang == 'ar'
     assert Document.objects.search('summary').count() == 5
     assert Document.objects.search('BSF').count() == 4
 
 
 @pytest.mark.parametrize('row', [
-    pytest.param('image,,my summary,BSF,an-image.jpg', id='missing-title'),
-    pytest.param('image,my title,my summary,BSF,', id='missing-path'),
+    pytest.param('image,,my summary,BSF,an-image.jpg,ar', id='missing-title'),
+    pytest.param('image,my title,my summary,BSF,,ar', id='missing-path'),
     pytest.param(
-        'image,my title,my summary,BSF,unknownpath.mp4', id='invalid-path'),
+        'image,my title,my summary,BSF,unknownpath.mp4,ar', id='invalid-path'),
+    pytest.param(
+        'image,my title,my summary,BSF,an-image.jpg,', id='missing-lang'),
 ])
 def test_should_skip_if_missing_required_metadata(row):
     assert not Document.objects.count()
-    metadata = ('kind,title,summary,credits,path\n' + row)
+    metadata = ('kind,title,summary,credits,path,lang\n' + row)
     write_metadata(metadata)
     with pytest.raises(SystemExit):
         call_command('import_medias', CSV_PATH)
@@ -55,8 +58,8 @@ def test_should_skip_if_missing_required_metadata(row):
 
 def test_should_cut_too_long_title():
     assert not Document.objects.count()
-    metadata = ('kind,title,summary,credits,path\n'
-                'image,This is a very long title with much much more than one hundrer chars so it should be cut to be imported,my summary,BSF,an-image.jpg')  # noqa
+    metadata = ('kind,title,summary,credits,path,lang\n'
+                'image,This is a very long title with much much more than one hundrer chars so it should be cut to be imported,my summary,BSF,an-image.jpg,ar')  # noqa
     write_metadata(metadata)
     call_command('import_medias', CSV_PATH)
     doc = Document.objects.last()
@@ -65,8 +68,8 @@ def test_should_cut_too_long_title():
 
 def test_should_not_reimport_if_already_existing():
     assert not Document.objects.count()
-    metadata = ('kind,title,summary,credits,path\n'
-                'video,my video,my video summary,BSF,a-video.mp4\n')
+    metadata = ('kind,title,summary,credits,path,lang\n'
+                'video,my video,my video summary,BSF,a-video.mp4,ar\n')
     write_metadata(metadata)
     call_command('import_medias', CSV_PATH)
     assert Document.objects.count() == 1
@@ -77,13 +80,13 @@ def test_should_not_reimport_if_already_existing():
 
 def test_should_update_if_already_existing_and_update_flag_used():
     assert not Document.objects.count()
-    metadata = ('kind,title,summary,credits,path\n'
-                'video,my video,my video summary,BSF,a-video.mp4\n')
+    metadata = ('kind,title,summary,credits,path,lang\n'
+                'video,my video,my video summary,BSF,a-video.mp4,ku\n')
     write_metadata(metadata)
     call_command('import_medias', CSV_PATH)
     assert Document.objects.count() == 1
-    metadata = ('kind,title,summary,credits,path\n'
-                'video,my video,another summary,BSF,a-video.mp4\n')
+    metadata = ('kind,title,summary,credits,path,lang\n'
+                'video,my video,another summary,BSF,a-video.mp4,ku\n')
     write_metadata(metadata)
     # Call again.
     call_command('import_medias', CSV_PATH, update=True)
@@ -93,8 +96,8 @@ def test_should_update_if_already_existing_and_update_flag_used():
 
 def test_should_guess_kind_from_path():
     assert not Document.objects.count()
-    metadata = ('title,summary,credits,path\n'
-                'my video,my video summary,BSF,a-video.mp4\n')
+    metadata = ('title,summary,credits,path,lang\n'
+                'my video,my video summary,BSF,a-video.mp4,ku\n')
     write_metadata(metadata)
     call_command('import_medias', CSV_PATH)
     assert Document.objects.count() == 1
@@ -107,8 +110,8 @@ def test_long_path():
     prefix = 'A' * 100
     long_path = prefix + '.mp4'
     long_preview = prefix + 'an-image.jpg'
-    metadata = ('title,summary,credits,path,preview\n'
-                'my video,my video summary,BSF,'
+    metadata = ('title,summary,credits,lang,path,preview\n'
+                'my video,my video summary,BSF,ku,'
                 + long_path + ',' + long_preview + '\n')
     write_metadata(metadata)
     call_command('import_medias', CSV_PATH)
@@ -119,8 +122,8 @@ def test_long_path():
 
 def test_should_add_preview_if_given():
     assert not Document.objects.count()
-    metadata = ('title,summary,credits,path,preview\n'
-                'my video,my video summary,BSF,a-video.mp4,an-image.jpg\n')
+    metadata = ('title,summary,credits,path,preview,lang\n'
+                'my video,my video summary,BSF,a-video.mp4,an-image.jpg,es\n')
     write_metadata(metadata)
     call_command('import_medias', CSV_PATH)
     assert Document.objects.count() == 1
@@ -129,8 +132,8 @@ def test_should_add_preview_if_given():
 
 def test_should_add_tags_if_given():
     assert not Document.objects.count()
-    metadata = ('title;summary;credits;path;tags\n'
-                'my video;my video summary;BSF;a-video.mp4;tag1,tag2')
+    metadata = ('title;summary;credits;path;tags;lang\n'
+                'my video;my video summary;BSF;a-video.mp4;tag1,tag2;es')
     write_metadata(metadata)
     call_command('import_medias', CSV_PATH)
     assert Document.objects.count() == 1
@@ -138,26 +141,13 @@ def test_should_add_tags_if_given():
     assert Document.objects.get(title='my video', tags__name="tag2")
 
 
-def test_should_honour_lang_if_given(settings):
-    settings.LANGUAGE_CODE = 'fr'
-    assert not Document.objects.count()
-    metadata = ('title,summary,credits,path,lang\n'
-                'my video,my video summary,BSF,a-video.mp4,ar\n'
-                'my image,my image summary,BSF,an-image.jpg,')
-    write_metadata(metadata)
-    call_command('import_medias', CSV_PATH)
-    assert Document.objects.count() == 2
-    assert Document.objects.get(title='my image').lang == 'fr'
-    assert Document.objects.get(title='my video').lang == 'ar'
-
-
 @pytest.mark.parametrize('dry_run', [False, True])
 def test_should_fail_nicely_if_path_doesn_t_exist(capsys, dry_run):
     assert not Document.objects.count()
-    metadata = ('title,summary,credits,path,preview\n'
-                'image, summary,BSF,an-image.jpg,not_existing0\n'
-                'image, summary,BSF,not_existing1,\n'
-                'image, summary,BSF,not_existing2,not_existing3\n')
+    metadata = ('title,summary,credits,path,preview,lang\n'
+                'image, summary,BSF,an-image.jpg,not_existing0,ar\n'
+                'image, summary,BSF,not_existing1,,es\n'
+                'image, summary,BSF,not_existing2,not_existing3,fr\n')
     write_metadata(metadata)
     with pytest.raises(SystemExit):
         call_command('import_medias', CSV_PATH, dry_run=dry_run)
@@ -170,10 +160,10 @@ def test_should_fail_nicely_if_path_doesn_t_exist(capsys, dry_run):
 @pytest.mark.parametrize('dry_run', [False, True])
 def test_should_fail_nicely_if_path_is_a_dir(capsys, dry_run):
     assert not Document.objects.count()
-    metadata = ('title,summary,credits,path,preview\n'
-                'image, summary,BSF,an-image.jpg,a-directory\n'
-                'image, summary,BSF,a-directory,\n'
-                'image, summary,BSF,a-directory,a-directory\n')
+    metadata = ('title,summary,credits,path,preview,lang\n'
+                'image, summary,BSF,an-image.jpg,a-directory,ar\n'
+                'image, summary,BSF,a-directory,,es\n'
+                'image, summary,BSF,a-directory,a-directory,fr\n')
     write_metadata(metadata)
     with pytest.raises(SystemExit):
         call_command('import_medias', CSV_PATH, dry_run=dry_run)
@@ -184,9 +174,9 @@ def test_should_fail_nicely_if_path_is_a_dir(capsys, dry_run):
 
 def test_import_media_allow_subdir():
     assert not Document.objects.count()
-    metadata = ('title,summary,credits,path,preview\n'
-                'video,summary,BSF,subdir/a-video.mp4,subdir/an-image.jpg\n'
-                'pdf,summary,BSF,subdir/a-pdf.pdf,subdir/an-image.jpg\n')
+    metadata = ('title,summary,credits,path,preview,lang\n'
+                'video,summary,BSF,subdir/a-video.mp4,subdir/an-image.jpg,ar\n'
+                'pdf,summary,BSF,subdir/a-pdf.pdf,subdir/an-image.jpg,es\n')
     write_metadata(metadata)
     call_command('import_medias', CSV_PATH)
     assert Document.objects.count() == 2
