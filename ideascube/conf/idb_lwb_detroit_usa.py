b1747378bfb0e238df0b16fdf262a0207fd40463
Mathieu Bridon <bochecha@daitauha.fr>
2017-09-28 15:27:15 +0200

    Don't overwrite a setting with the default value

--- ideascube/conf/idb_lwb_detroit_usa.py
+++ ideascube/conf/idb_lwb_detroit_usa.py
@@ -10,7 +10,6 @@ IDEASCUBE_NAME = u"Library Whitout Border Detroit"
 IDEASCUBE_PLACE_NAME = _("city")
 COUNTRIES_FIRST = ['US']
 TIME_ZONE = get_localzone().zone
-LANGUAGE_CODE = 'en'
 LOAN_DURATION = 14
 MONITORING_ENTRY_EXPORT_FIELDS = ['serial', 'user_id', 'birth_year', 'gender']
 USER_FORM_FIELDS = (
