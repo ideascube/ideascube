ca3f76f94a07e239cb998dfe12ffecc0cdaa7f5b
Mathieu Bridon <bochecha@daitauha.fr>
2018-03-08 11:18:37 +0100

    conf: Remove definitions of DOMAIN and ALLOWED_HOSTS
    Now that deployments can define it through an environment variable,
    let's remove these which became obsolete.

--- ideascube/conf/idb_col_llavedelsaber.py
+++ ideascube/conf/idb_col_llavedelsaber.py
@@ -3,8 +3,6 @@ from .idb import *  # pragma: no flakes
 from django.utils.translation import ugettext_lazy as _
 
 LANGUAGE_CODE = 'es'
-DOMAIN = 'bibliotecamovil.lan'
-ALLOWED_HOSTS = ['.bibliotecamovil.lan', 'localhost']
 
 USER_FORM_FIELDS = USER_FORM_FIELDS + (  # pragma: no flakes
         (_('Personal informations'), ['disabilities']),
--- ideascube/conf/kb.py
+++ ideascube/conf/kb.py
@@ -3,9 +3,7 @@ from .base import *  # pragma: no flakes
 from tzlocal import get_localzone
 
 
-ALLOWED_HOSTS = ['.koombook.lan', 'localhost', '127.0.0.1']
 TIME_ZONE = get_localzone().zone
-DOMAIN = 'koombook.lan'
 BACKUP_FORMAT = 'gztar'
 STAFF_HOME_CARDS = [c for c in STAFF_HOME_CARDS  # pragma: no flakes
                     if c['url'] in ['user_list', 'server:settings']]
