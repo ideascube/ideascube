8e1e76746bbacdfeb2e6f02509b76d9de7802ba9
fheslouin <florian.heslouin@bibliosansfrontieres.org>
2018-09-25 15:48:39 +0200

    0.37.3

--- ideascube/__init__.py
+++ ideascube/__init__.py
@@ -4,6 +4,6 @@ if sys.version_info.major != 3:
     sys.exit('ERROR: Ideascube only works with Python 3')
 
 
-VERSION = (0, 37, 2)
+VERSION = (0, 37, 3)
 
 __version__ = ".".join(map(str, VERSION))
